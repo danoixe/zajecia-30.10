5e3b2f174c2b4a2c35c088a9ea8b478043911323
test <test@Delli5>
2017-11-03 20:39:27 +0100

    Zadanie 6

--- src/Zadanie_2_6.java
+++ src/Zadanie_2_6.java
@@ -7,6 +7,8 @@ public class Zadanie_2_6 {
 		Random los = new Random();
 		Scanner reader = new Scanner(System.in);
 		int a = los.nextInt(100)+1;
+		System.out.println("Gra w 'Za du¿o, za ma³o'");
+		System.out.println("Podaj liczbê od 1 do 100");
 		int b = reader.nextInt();
 		while (b!=a) {
 			if (b<a) {
